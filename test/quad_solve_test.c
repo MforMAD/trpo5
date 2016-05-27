f59d26e5f7a0e30fc1ece92ebcdd0f741f50e6d7
MforMad <smb.like.you@gmail.com>
2016-05-28 02:05:43 +0600

    Add two_roots_test

--- test/quad_solve_test.c
+++ test/quad_solve_test.c
@@ -16,3 +16,20 @@ CTEST(quad_solve_suite, koef_a_equal_to_zero_test) {
 
     ASSERT_EQUAL(expected_result.count, result.count);
 }
+
+CTEST(distance_suite, two_roots_test) {
+    // Given
+    const float a = 7;
+    const float b = 13;
+    const float c = 2;
+
+    // When
+    const Quad_root result = quad_solve(a, b, c);
+
+    // Then
+    const Quad_root expected_result = {-0.169275299 , -1.687867558, TWO_ROOTS};
+
+    ASSERT_EQUAL(expected_result.count, result.count);
+    ASSERT_DBL_NEAR(expected_result.x1, result.x1);
+    ASSERT_DBL_NEAR(expected_result.x2, result.x2);
+}
